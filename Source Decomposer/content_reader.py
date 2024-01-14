1198be87b2036c66458695e73d4029c42c4cc22a
BlueDragonFire <wikusvh88@gmail.com>
2024-01-14 18:38:51 +0200

    Fixed content_reader error and now it works properly

--- Source Decomposer/content_reader.py
+++ Source Decomposer/content_reader.py
@@ -1,4 +1,17 @@
-
+import re
 
 class content_reader:
-    a
+
+    def read_file(self, path):
+        open_file = open(path, 'r')
+        file_contents = open_file.read()
+        return file_contents
+
+    def find_functions(self, text):
+        x = re.findall("def.*:", text)
+        return x
+
+    def get_functions(self, text):
+        a = self.read_file(text)
+        b = self.find_functions(a)
+        return b
