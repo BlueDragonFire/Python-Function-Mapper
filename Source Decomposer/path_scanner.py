c4dd6c238a345fdc94cd67955a6bd5377cc7ba56
BlueDragonFire <wikusvh88@gmail.com>
2024-01-17 22:25:29 +0200

    Made minor changes and fixed path_scanner so now it returns an appropriate list for jsonifying

--- Source Decomposer/path_scanner.py
+++ Source Decomposer/path_scanner.py
@@ -1,21 +1,25 @@
 #This class is for reading through a path and creating a map of all files and folders therein
 
 import os
+from json_builder import *
 
 class path_scanner:
 
 
     def BFS(self, path):
-        print('')
+        
+        result = []
         for (root,dirs,files) in os.walk(path):
+            temp = []
             dirs[:] = [d for d in dirs if not d.startswith('.')]
-            files[:] = [f for f in files if f.endswith('.py') | f.endswith('.json')]
-            print(root)
-            print(dirs)
-            print(files)
-            print("=======================")
-
+            files[:] = [f for f in files if f.endswith('.py')]
+            temp.extend((root, dirs, files))
+            result.append(temp)
+        
+        return result
 
 a = path_scanner()
-
+a2 = json_builder()
 b = a.BFS("/home/heph/Python/yt-dlp")
+#print(b)
+a2.create_json_file(a2.jsonify(b))
